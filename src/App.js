bda27403e67e0014d750d5e5589f73442c0e40ec
Elias Iinatti <n3iiel00@students.oamk.fi>
2024-11-07 14:34:40 +0200

    Initial commit of my React app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the React App</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
